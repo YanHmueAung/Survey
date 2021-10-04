e215fd4ef223cab8886e879719c9acb9a80db997
yan <yanhmueaung@gmail.com>
2021-10-04 22:05:57 +0800

    Finish of auth on Client Site

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import authReducer from "../reducers/authReducer";
+import { Link } from "react-router-dom";
+
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login with google</a></li>;
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
     render() {
+        console.log(this.props);
         return (
             <div>
                 <nav>
                     <div className="nav-wrapper">
-                        <a href="#" className="brand-logo">Emaily</a>
+                        <Link
+                            to={this.props.auth ? '/surveys' : '/'}
+                            href="#"
+                            className="brand-logo"
+                        >
+                            Emaily
+                        </Link>
                         <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><a href="/auth/google">Login in with google</a></li>
+                            {this.renderContent()}
+
                         </ul>
                     </div>
                 </nav>
@@ -17,4 +39,7 @@ class Header extends Component {
         )
     }
 }
-export default Header;
+function mapStateToProps(state) {
+    return { auth: state.auth };
+}
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
+import { FETCH_USER } from "../actions/types";
 export default function authReducer(state = {}, action) {
     console.log(action);
     switch (action.type) {
+        case FETCH_USER:
+            return action.payload || false;//'' || false >> false
         default:
             return state;
     }
