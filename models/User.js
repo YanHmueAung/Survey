0d2325dda0579ec9975005f239537df4936fa7d3
yan <yanhmueaung@gmail.com>
2021-10-05 18:00:54 +0800

    Required login middleware with next

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
-const mongoose=require('mongoose');
-const {Schema}=mongoose;
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const userSchema=new Schema({
-    googleId:String
+const userSchema = new Schema({
+    googleId: String,
+    credits: { type: Number, default: 0 }
 })
-mongoose.model('users',userSchema);
+mongoose.model('users', userSchema);
