cf894c83c5c424425c9982ad0c41c71b898603eb
yan <yanhmueaung@gmail.com>
2021-10-16 17:22:24 +0800

    Getting back all surveys to view

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,7 +9,11 @@ const Survey = mongoose.model('surveys');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 module.exports = app => {
-
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+        const surveys = await Survey.find({ _user: req.user.id })
+            .select({ recipients: false });
+        res.send(surveys);
+    })
     app.get('/api/surveys/:surveyId/:choice', async (req, res) => {
         res.send('Thanks for your surverys');
     })
